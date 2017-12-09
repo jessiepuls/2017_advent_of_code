9092143d4dba0ad90e2742e136deb7bfbc88318a
Jessie Puls <jessiepuls@gmail.com>
2017-12-09 12:27:14 -0600

    Adding logic to make it look at the next index when adding

--- inverse_captcha/inverse_captcha.rb
+++ inverse_captcha/inverse_captcha.rb
@@ -1,4 +1,10 @@
 def inverse_captcha(str)
-  return 0 if str == ''
-  1
+  result = 0
+
+  strval = str.split('').map(&:to_i)
+  strval.each.with_index(1) { |value, index|
+    result += value if value == strval[index]
+  }
+
+  result
 end
--- inverse_captcha/inverse_captcha_spec.rb
+++ inverse_captcha/inverse_captcha_spec.rb
@@ -6,7 +6,11 @@ describe 'inverse captcha' do
     expect(inverse_captcha('')).to be 0
   end
 
-  it 'should return 1 for 11' do
-    expect(inverse_captcha('11')).to be 1
+  it 'should return 1 for 112' do
+    expect(inverse_captcha('112')).to be 1
+  end
+
+  it 'should return 2 for 223' do
+    expect(inverse_captcha('223')).to be 2
   end
 end
