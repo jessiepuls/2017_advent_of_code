ca0e0f12230406fc858bd8e7e596be2105ec3be5
Jessie Puls <jessiepuls@gmail.com>
2017-12-10 17:51:50 -0600

    Refactoring to allow an offset to be passed in and set index in "circular list" from the beginning of the string using that offset

--- inverse_captcha/inverse_captcha.rb
+++ inverse_captcha/inverse_captcha.rb
@@ -1,9 +1,9 @@
-def inverse_captcha(str)
+def inverse_captcha(str, offset = 1)
   result = 0
 
   strval = str.split('').map(&:to_i)
-  strval.each.with_index(1) { |value, index|
-    index = 0 if index == strval.length
+  strval.each.with_index(offset) { |value, index|
+    index -= strval.length if index >= strval.length
     result += value if value == strval[index]
   }
 
