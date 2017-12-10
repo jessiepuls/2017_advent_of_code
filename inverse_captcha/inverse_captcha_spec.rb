14e2c87e5e13d90c2bf9f5af6ca29b06cbb71d14
Jessie Puls <jessiepuls@gmail.com>
2017-12-10 17:55:00 -0600

    Adding other test cases for different offsets

--- inverse_captcha/inverse_captcha_spec.rb
+++ inverse_captcha/inverse_captcha_spec.rb
@@ -17,4 +17,16 @@ describe 'inverse captcha' do
   it 'should return 4 for 22' do
     expect(inverse_captcha('22')).to be 4
   end
+
+  it 'should return 6 for 1212' do
+    expect(inverse_captcha('1212', 2)).to be 6
+  end
+
+  it 'should return 0 for 1221' do
+    expect(inverse_captcha('1221', 2)).to be 0
+  end
+
+  it 'should return 4 for 123425' do
+    expect(inverse_captcha('123425', 3)).to be 4
+  end
 end
