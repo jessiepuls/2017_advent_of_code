3b82b03c28a2310211cd099992883e376a037045
Jessie Puls <jessiepuls@gmail.com>
2017-12-10 22:12:31 -0600

    Testing that checksums are calculated correclty using the divide method

--- corruption_checksum/corruption_checksum_spec.rb
+++ corruption_checksum/corruption_checksum_spec.rb
@@ -3,6 +3,7 @@ require_relative 'corruption_checksum'
 
 describe 'corruption checksum' do
   let(:min_max) { CorruptionChecksum.method(:min_max) }
+  let(:divide) { CorruptionChecksum.method(:divide) }
 
   context 'min_max' do
     it 'should return 8 for an input of 5 1 9 5' do
@@ -29,14 +30,19 @@ describe 'corruption checksum' do
   end
 
   context 'checksum' do
-    it 'should return 18 for "5 1 9 5\n7 5 3\n2 4 6 8"' do
+    it 'should return 18 for "5 1 9 5\n7 5 3\n2 4 6 8" using min_max' do
       input = "5 1 9 5\n7 5 3\n2 4 6 8"
       expect(CorruptionChecksum.checksum(input, &min_max)).to be 18
     end
 
-    it 'should return 20 for "5 1 9 5\n7 5 2\n1 4 6 8"' do
+    it 'should return 20 for "5 1 9 5\n7 5 2\n1 4 6 8" using min_max' do
       input = "5 1 9 5\n7 5 2\n1 4 6 8"
       expect(CorruptionChecksum.checksum(input, &min_max)).to be 20
+    end
+
+    it 'should return 9 for "5 9 2 8\n9 4 7 3\n3 8 6 5" using divide' do
+      input = "5 9 2 8\n9 4 7 3\n3 8 6 5"
+      expect(CorruptionChecksum.checksum(input, &divide)).to be 9
     end
   end
 end
