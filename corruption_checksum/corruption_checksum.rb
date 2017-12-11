e6f3e6bf227ee71da48a13c375b3d93d7af817b1
Jessie Puls <jessiepuls@gmail.com>
2017-12-10 18:31:27 -0600

    Adding checksum function and setting up initial test

--- corruption_checksum/corruption_checksum.rb
+++ corruption_checksum/corruption_checksum.rb
@@ -1,4 +1,8 @@
 class CorruptionChecksum
+  def checksum(input)
+    18
+  end
+
   def row_val(input)
     tokenized_input = input.split(' ').map(&:to_i)
     tokenized_input.max - tokenized_input.min
--- corruption_checksum/corruption_checksum_spec.rb
+++ corruption_checksum/corruption_checksum_spec.rb
@@ -12,5 +12,16 @@ describe 'corruption checksum' do
     it 'should return 4 for an input of 7 5 3' do
       expect(corruption_checksum.row_val('7 5 3')).to be 4
     end
+
+    it 'should return 6 for an input of 2 4 6 8' do
+      expect(corruption_checksum.row_val('2 4 6 8')).to be 6
+    end
+  end
+
+  context 'checksum' do
+    it 'should return a sum of each row val' do
+      input = "5 1 9 5\n7 5 3\n2 4 6 8"
+      expect(corruption_checksum.checksum(input)).to be 18
+    end
   end
 end
