0d92ef156fb6bc4af8848657f1b2dd549b74b2a5
Jessie Puls <jessiepuls@gmail.com>
2017-12-10 22:22:45 -0600

    extracting logic to tokenize input into a method

--- corruption_checksum/corruption_checksum.rb
+++ corruption_checksum/corruption_checksum.rb
@@ -1,24 +1,27 @@
 class CorruptionChecksum
   def self.checksum(input)
-    tokenized_input = input.split("\n")
-    tokenized_input.map {|row| yield(row)}.sum
+    input.split("\n").map { |row| yield(row) }.sum
   end
 
-  def self.min_max(input)
-    tokenized_input = input.split(' ').map(&:to_i)
-    tokenized_input.max - tokenized_input.min
+  def self.min_max(row)
+    row_values = tokenize_input(row)
+    row_values.max - row_values.min
   end
 
-  def self.divide(input)
-    tokenized_input = input.split(' ').map(&:to_i)
+  def self.divide(row)
+    row_values = tokenize_input(row)
 
-    tokenized_input.each_with_index { |val, index|
-      tokenized_input.each_with_index { |divisor, divisor_index|
+    row_values.each_with_index { |val, index|
+      row_values.each_with_index { |divisor, divisor_index|
         next if index == divisor_index
         return val / divisor if (val % divisor).zero?
       }
     }
 
     0
+  end
+
+  def self.tokenize_input(input)
+    input.split(' ').map(&:to_i)
   end
 end
