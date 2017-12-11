53e9e25186ba51b16198f649126cbd7926f091cc
Jessie Puls <jessiepuls@gmail.com>
2017-12-10 18:26:15 -0600

    Updating row_val to tokenize input and return the difference between the max and min values

--- corruption_checksum/corruption_checksum.rb
+++ corruption_checksum/corruption_checksum.rb
@@ -1,5 +1,6 @@
 class CorruptionChecksum
   def row_val(input)
-    8
+    tokenized_input = input.split(' ').map(&:to_i)
+    tokenized_input.max - tokenized_input.min
   end
 end
--- corruption_checksum/corruption_checksum_spec.rb
+++ corruption_checksum/corruption_checksum_spec.rb
@@ -5,8 +5,12 @@ describe 'corruption checksum' do
   let(:corruption_checksum) { CorruptionChecksum.new }
 
   context 'row_val' do
-    it 'should return the difference between the largest and smallest value' do
+    it 'should return 8 for an input of 5 1 9 5' do
       expect(corruption_checksum.row_val('5 1 9 5')).to be 8
+    end
+
+    it 'should return 4 for an input of 7 5 3' do
+      expect(corruption_checksum.row_val('7 5 3')).to be 4
     end
   end
 end
